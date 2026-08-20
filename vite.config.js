d5ad2ee4576ad6655e88cee0e09987eda6faf7b0
Walid <waliddev22@gmail.com>
2026-08-20 20:18:21 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/demo-thebonfire/',
+    base: '/',
     plugins: [
         tailwindcss(),
     ],
