787ffbecdb84ef5feb94518a8bbbff1135796545
Walid <waliddev22@gmail.com>
2026-08-20 20:27:26 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/',
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     plugins: [
         tailwindcss(),
     ],
